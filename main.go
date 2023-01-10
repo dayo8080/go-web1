7722fb729b297c774f2eda872937d96f2daa36af
Dayo Oluseye <dayo.oluseye@gmail.com>
2023-01-10 17:54:45 +0100

    templates

--- main.go
+++ main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"errors"
 	"fmt"
+	"html/template"
 	"net/http"
 )
 
@@ -12,7 +12,6 @@ func main() {
 
 	http.HandleFunc("/", Home)
 	http.HandleFunc("/about", About)
-	http.HandleFunc("/divide", Divide)
 
 	fmt.Println(fmt.Sprintf("The server is running on port %s", portNumber))
 	http.ListenAndServe(portNumber, nil)
@@ -20,35 +19,19 @@ func main() {
 }
 
 func Home(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "This is the home page")
+	renderTemplate(w, "home.page.tmpl")
+
 }
 
 func About(w http.ResponseWriter, r *http.Request) {
-	sum := addValues(2, 6)
-	_, _ = fmt.Fprintf(w, fmt.Sprintf("This is about page, the addition of 2 + 6 = %d", sum))
+	renderTemplate(w, "about.page.tmpl")
 }
 
-func addValues(x, y int) int {
-	return x + y
-}
 
-func Divide(w http.ResponseWriter, r *http.Request) {
-	x, y := 100.0, 0.0
-	f, err := divideValues(x, y)
+func renderTemplate(w http.ResponseWriter, tmpl string) {
+	parsedTemplated, _ := template.ParseFiles("./templates/" + tmpl)
+	err := parsedTemplated.Execute(w, nil)
 	if err != nil {
-		fmt.Fprintf(w, "Cannot divide by zero")
-		return
+		fmt.Println("error parsing templates:", err)
 	}
-	fmt.Fprintf(w, fmt.Sprintf("%f divided by %f is %f", x, y, f))
-}
-
-func divideValues(x, y float64) (float64, error) {
-	if y <= 0 {
-		err := errors.New("CANNOT DIVIDE BY ZERO")
-		return 0, err
-	}
-	result := x / y
-
-	return result, nil
-
 }
